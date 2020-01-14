32a68cc2fd8ebc5999efd94cfc76eeb59a9e8231
codefvcker <alehsiarheyeu@gmail.com>
2020-01-14 16:54:00 +0300

    Add local storage and other

--- src/app.js
+++ src/app.js
@@ -1,3 +1,39 @@
+import { Question } from './question';
+import { isValid } from './utils';
 import './style.css';
 
-console.log('app working')
+
+const form = document.getElementById('form');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+
+window.addEventListener('load', Question.renderList);
+form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(e) {
+    e.preventDefault();
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true;
+
+        // Async request to server to save question
+        Question.create(question).then(() => {
+            input.value = '';
+            input.className = '';
+            submitBtn.disabled = false;
+        });
+
+
+
+    }
+}
+
+
---
+++ src/question.js
@@ -0,0 +1,51 @@
+export class Question {
+    static create(question) {
+        return fetch('https://js-auth-app-19856.firebaseio.com/questions.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(question),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(response => {
+                question.id = response.name
+                return question
+            })
+            .then(addToLocalStorage)
+            .then(Question.renderList)
+    }
+
+    static renderList() {
+        const allQuestions = getQuestionsFromStorage()
+
+        const html = allQuestions.length
+            ? allQuestions.map(toCard).join('')
+            : `<div class="mui--text-headline">There are no questions</div>`
+
+        const list = document.getElementById('list');
+        list.innerHTML = html;
+    }
+}
+
+function addToLocalStorage(question) {
+    const allQuestions = getQuestionsFromStorage()
+    allQuestions.push(question)
+    localStorage.setItem('questions', JSON.stringify(allQuestions))
+}
+
+function getQuestionsFromStorage() {
+    return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+
+function toCard(question) {
+    return `<div class="mui--text-black-54">
+        ${new Date(question.date).toLocaleDateString()}
+        ${new Date(question.date).toLocaleTimeString()}
+    </div>
+    <div>
+     ${question.text}
+    </div>
+    <br>`
+}
